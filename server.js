0ba126621d94479ebd8187927d5beb5595a81b3e
nachofreyre <nachofreyre41@gmail.com>
2026-05-27 20:30:11 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("PUBLIC"));
 
 app.post("/api/chat", async (req, res) => {
   const { prompt, system } = req.body;
